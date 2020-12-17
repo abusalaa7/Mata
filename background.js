905f53d389ae793be85cfe34f79b228eb11c9cf0
Ahmed Mansour <a.salah.mansour@gmail.com>
2020-12-16 21:37:59 -0500

    Changed css + update all tabs

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+  chrome.tabs.query({}, function(tabs) {
+    tabs.forEach(tab => {
+      chrome.tabs.sendMessage(tab.id, { "message": "clicked_browser_action" });
+    });
   });
 });
 
